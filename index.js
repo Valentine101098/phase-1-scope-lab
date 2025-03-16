794776cff6fc5f3f962d5cb198ba6979ba84bb7c
Valentine Wanjiru <kt116tabitha@gmail.com>
2025-03-17 00:47:30 +0300

    Using scope to access and change variables

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob';
+// var bestCustomer = 'not bob';
+const leastFavoriteCustomer = 'val';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    globalThis.bestCustomer = 'not bob';
+  }
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'ben';
+}
